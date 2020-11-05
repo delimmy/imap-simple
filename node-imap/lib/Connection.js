070d03f8db4628c93796d20c08b1d8991c939558
liteCarma <litecarma@gmail.com>
2020-11-05 19:03:18 +0700

    fix tls for proxy

--- node-imap/lib/Connection.js
+++ node-imap/lib/Connection.js
@@ -63,7 +63,7 @@ function Connection(config) {
     host: config.host || 'localhost',
     port: config.port || 143,
     tls: config.tls,
-    tlsOptions: config.tlsOptions,
+    tlsOptions: config.tlsOptions || {servername: config.host},
     autotls: config.autotls,
     user: config.user,
     password: config.password,
@@ -96,234 +96,223 @@ function Connection(config) {
 inherits(Connection, EventEmitter);
 
 Connection.prototype.connect = async function() {
-  const config = this._config;
+    const config = this._config;
+    const self = this
 
-  if (config.proxy) {
-    const options = {
-      proxy: config.proxy,
-      command: 'connect', // connect, bind, associate
-      destination: {
-        host: config.host, // ipv4, ipv6, or hostname
-        port: config.port
-      } 
+    let socket = config.socket || new Socket();
+    socket.setKeepAlive(true);
+    this._sock = undefined;
+    this._tagcount = 0;
+    this._tmrConn = undefined;
+    this._tmrKeepalive = undefined;
+    this._tmrAuth = undefined;
+    this._queue = [];
+    this._box = undefined;
+    this._idle = { started: undefined, enabled: false };
+    this._parser = undefined;
+    this._curReq = undefined;
+    this.delimiter = undefined;
+    this.namespaces = undefined;
+    this.state = 'disconnected';
+
+    if (config.proxy) {
+      const options = {
+        proxy: config.proxy,
+        command: 'connect', // connect, bind, associate
+        destination: {
+          host: config.host, // ipv4, ipv6, or hostname
+          port: config.port
+        }
+      }
+
+      try {
+        const info = await SocksClient.createConnection(options)
+        socket = info.socket
+      } catch (e) {
+        this.emit('error', e);
+        return
+      }
+    } else {
+      socket = config.socket || new Socket();
+    }
+
+    socket.setKeepAlive(true);
+
+    if (config.tls) {
+      tlsOptions = {};
+      tlsOptions.host = config.host;
+      // Host name may be overridden the tlsOptions
+      for (var k in config.tlsOptions)
+        tlsOptions[k] = config.tlsOptions[k];
+      tlsOptions.socket = socket;
+
+      this._sock = tls.connect(tlsOptions, onconnect);
+    } else {
+      socket.once('connect', onconnect);
+      this._sock = socket;
+    }
+
+    function onconnect() {
+      clearTimeout(self._tmrConn);
+      self.state = 'connected';
+      self.debug && self.debug('[connection] Connected to host');
+      self._tmrAuth = setTimeout(function() {
+        var err = new Error('Timed out while authenticating with server');
+        err.source = 'timeout-auth';
+        self.emit('error', err);
+        socket.destroy();
+      }, config.authTimeout);
     }
   
-    try {
-      SocksClient.createConnection(options, (err, info) => {
-        if (err) {
-          this.emit('error', err);
-          return
-        }
-        config.socket = info.socket;
-        connect.call(this, config)
-      })
-    } catch (err) {
-      this.emit('error', err);
-    }
-
-    
-  } else {
-    connect.call(this, config)
-  }
-};
-
-function connect (config) {
-  const self = this;
-  let tlsOptions, socket, parser;
-
-
-  socket = config.socket || new Socket();
-  socket.setKeepAlive(true);
-  this._sock = undefined;
-  this._tagcount = 0;
-  this._tmrConn = undefined;
-  this._tmrKeepalive = undefined;
-  this._tmrAuth = undefined;
-  this._queue = [];
-  this._box = undefined;
-  this._idle = { started: undefined, enabled: false };
-  this._parser = undefined;
-  this._curReq = undefined;
-  this.delimiter = undefined;
-  this.namespaces = undefined;
-  this.state = 'disconnected';
-
-  if (config.tls) {
-    tlsOptions = {};
-    tlsOptions.host = config.host;
-    // Host name may be overridden the tlsOptions
-    for (var k in config.tlsOptions)
-      tlsOptions[k] = config.tlsOptions[k];
-    tlsOptions.socket = socket;
-  }
-
-  if (config.tls)
-    this._sock = tls.connect(tlsOptions, onconnect);
-  else {
-    socket.once('connect', onconnect);
-    this._sock = socket;
-  }
-
-  function onconnect() {
-    clearTimeout(self._tmrConn);
-    self.state = 'connected';
-    self.debug && self.debug('[connection] Connected to host');
-    self._tmrAuth = setTimeout(function() {
-      var err = new Error('Timed out while authenticating with server');
-      err.source = 'timeout-auth';
+    this._onError = function(err) {
+      clearTimeout(self._tmrConn);
+      clearTimeout(self._tmrAuth);
+      self.debug && self.debug('[connection] Error: ' + err);
+      err.source = 'socket';
+      self.emit('error', err);
+    };
+    this._sock.on('error', this._onError);
+  
+    this._onSocketTimeout = function() {
+      clearTimeout(self._tmrConn);
+      clearTimeout(self._tmrAuth);
+      clearTimeout(self._tmrKeepalive);
+      self.state = 'disconnected';
+      self.debug && self.debug('[connection] Socket timeout');
+  
+      var err = new Error('Socket timed out while talking to server');
+      err.source = 'socket-timeout';
       self.emit('error', err);
       socket.destroy();
-    }, config.authTimeout);
-  }
-
-  this._onError = function(err) {
-    clearTimeout(self._tmrConn);
-    clearTimeout(self._tmrAuth);
-    self.debug && self.debug('[connection] Error: ' + err);
-    err.source = 'socket';
-    //self.emit('error', err);
-  };
-  this._sock.on('error', this._onError);
-
-  this._onSocketTimeout = function() {
-    clearTimeout(self._tmrConn);
-    clearTimeout(self._tmrAuth);
-    clearTimeout(self._tmrKeepalive);
-    self.state = 'disconnected';
-    self.debug && self.debug('[connection] Socket timeout');
-
-    var err = new Error('Socket timed out while talking to server');
-    err.source = 'socket-timeout';
-    self.emit('error', err);
-    socket.destroy();
-  };
-  this._sock.on('timeout', this._onSocketTimeout);
-  socket.setTimeout(config.socketTimeout);
-
-  socket.once('close', function(had_err) {
-    clearTimeout(self._tmrConn);
-    clearTimeout(self._tmrAuth);
-    clearTimeout(self._tmrKeepalive);
-    self.state = 'disconnected';
-    self.debug && self.debug('[connection] Closed');
-    self.emit('close', had_err);
-  });
-
-  socket.once('end', function() {
-    clearTimeout(self._tmrConn);
-    clearTimeout(self._tmrAuth);
-    clearTimeout(self._tmrKeepalive);
-    self.state = 'disconnected';
-    self.debug && self.debug('[connection] Ended');
-    self.emit('end');
-  });
-
-  this._parser = parser = new Parser(this._sock, this.debug);
-
-  parser.on('untagged', function(info) {
-    self._resUntagged(info);
-  });
-  parser.on('tagged', function(info) {
-    self._resTagged(info);
-  });
-  parser.on('body', function(stream, info) {
-    var msg = self._curReq.fetchCache[info.seqno], toget;
-
-    if (msg === undefined) {
-      msg = self._curReq.fetchCache[info.seqno] = {
-        msgEmitter: new EventEmitter(),
-        toget: self._curReq.fetching.slice(0),
-        attrs: {},
-        ended: false
-      };
-
-      self._curReq.bodyEmitter.emit('message', msg.msgEmitter, info.seqno);
-    }
-
-    toget = msg.toget;
-
-    // here we compare the parsed version of the expression inside BODY[]
-    // because 'HEADER.FIELDS (TO FROM)' really is equivalent to
-    // 'HEADER.FIELDS ("TO" "FROM")' and some servers will actually send the
-    // quoted form even if the client did not use quotes
-    var thisbody = parseExpr(info.which);
-    for (var i = 0, len = toget.length; i < len; ++i) {
-      if (_deepEqual(thisbody, toget[i])) {
-        toget.splice(i, 1);
-        msg.msgEmitter.emit('body', stream, info);
-        return;
-      }
-    }
-    stream.resume(); // a body we didn't ask for?
-  });
-  parser.on('continue', function(info) {
-    var type = self._curReq.type;
-    if (type === 'IDLE') {
-      if (self._queue.length
-          && self._idle.started === 0
-          && self._curReq
-          && self._curReq.type === 'IDLE'
-          && self._sock
-          && self._sock.writable
-          && !self._idle.enabled) {
-        self.debug && self.debug('=> DONE');
-        self._sock.write('DONE' + CRLF);
-        return;
-      }
-      // now idling
-      self._idle.started = Date.now();
-    } else if (/^AUTHENTICATE XOAUTH/.test(self._curReq.fullcmd)) {
-      self._curReq.oauthError = new Buffer(info.text, 'base64').toString('utf8');
-      self.debug && self.debug('=> ' + inspect(CRLF));
-      self._sock.write(CRLF);
-    } else if (type === 'APPEND') {
-      self._sockWriteAppendData(self._curReq.appendData);
-    } else if (self._curReq.lines && self._curReq.lines.length) {
-      var line = self._curReq.lines.shift() + '\r\n';
-      self.debug && self.debug('=> ' + inspect(line));
-      self._sock.write(line, 'binary');
-    }
-  });
-  parser.on('other', function(line) {
-    var m;
-    if (m = RE_IDLENOOPRES.exec(line)) {
-      // no longer idling
-      self._idle.enabled = false;
-      self._idle.started = undefined;
+    };
+    this._sock.on('timeout', this._onSocketTimeout);
+    socket.setTimeout(config.socketTimeout);
+  
+    socket.once('close', function(had_err) {
+      clearTimeout(self._tmrConn);
+      clearTimeout(self._tmrAuth);
       clearTimeout(self._tmrKeepalive);
-
-      self._curReq = undefined;
-
-      if (self._queue.length === 0
-          && self._config.keepalive
-          && self.state === 'authenticated'
-          && !self._idle.enabled) {
-        self._idle.enabled = true;
-        if (m[1] === 'NOOP')
-          self._doKeepaliveTimer();
-        else
-          self._doKeepaliveTimer(true);
-      }
-
-      self._processQueue();
-    }
-  });
-
-  this._tmrConn = setTimeout(function() {
-    var err = new Error('Timed out while connecting to server');
-    err.source = 'timeout';
-    self.emit('error', err);
-    socket.destroy();
-  }, config.connTimeout);
-
-  if (!config.proxy) {
-    socket.connect({
-      port: config.port,
-      host: config.host,
-      localAddress: config.localAddress
+      self.state = 'disconnected';
+      self.debug && self.debug('[connection] Closed');
+      self.emit('close', had_err);
     });
-  }
-}
+  
+    socket.once('end', function() {
+      clearTimeout(self._tmrConn);
+      clearTimeout(self._tmrAuth);
+      clearTimeout(self._tmrKeepalive);
+      self.state = 'disconnected';
+      self.debug && self.debug('[connection] Ended');
+      self.emit('end');
+    });
+  
+    this._parser = parser = new Parser(this._sock, this.debug);
+  
+    parser.on('untagged', function(info) {
+      self._resUntagged(info);
+    });
+    parser.on('tagged', function(info) {
+      self._resTagged(info);
+    });
+    parser.on('body', function(stream, info) {
+      var msg = self._curReq.fetchCache[info.seqno], toget;
+  
+      if (msg === undefined) {
+        msg = self._curReq.fetchCache[info.seqno] = {
+          msgEmitter: new EventEmitter(),
+          toget: self._curReq.fetching.slice(0),
+          attrs: {},
+          ended: false
+        };
+  
+        self._curReq.bodyEmitter.emit('message', msg.msgEmitter, info.seqno);
+      }
+  
+      toget = msg.toget;
+  
+      // here we compare the parsed version of the expression inside BODY[]
+      // because 'HEADER.FIELDS (TO FROM)' really is equivalent to
+      // 'HEADER.FIELDS ("TO" "FROM")' and some servers will actually send the
+      // quoted form even if the client did not use quotes
+      var thisbody = parseExpr(info.which);
+      for (var i = 0, len = toget.length; i < len; ++i) {
+        if (_deepEqual(thisbody, toget[i])) {
+          toget.splice(i, 1);
+          msg.msgEmitter.emit('body', stream, info);
+          return;
+        }
+      }
+      stream.resume(); // a body we didn't ask for?
+    });
+    parser.on('continue', function(info) {
+      var type = self._curReq.type;
+      if (type === 'IDLE') {
+        if (self._queue.length
+            && self._idle.started === 0
+            && self._curReq
+            && self._curReq.type === 'IDLE'
+            && self._sock
+            && self._sock.writable
+            && !self._idle.enabled) {
+          self.debug && self.debug('=> DONE');
+          self._sock.write('DONE' + CRLF);
+          return;
+        }
+        // now idling
+        self._idle.started = Date.now();
+      } else if (/^AUTHENTICATE XOAUTH/.test(self._curReq.fullcmd)) {
+        self._curReq.oauthError = new Buffer(info.text, 'base64').toString('utf8');
+        self.debug && self.debug('=> ' + inspect(CRLF));
+        self._sock.write(CRLF);
+      } else if (type === 'APPEND') {
+        self._sockWriteAppendData(self._curReq.appendData);
+      } else if (self._curReq.lines && self._curReq.lines.length) {
+        var line = self._curReq.lines.shift() + '\r\n';
+        self.debug && self.debug('=> ' + inspect(line));
+        self._sock.write(line, 'binary');
+      }
+    });
+    parser.on('other', function(line) {
+      var m;
+      if (m = RE_IDLENOOPRES.exec(line)) {
+        // no longer idling
+        self._idle.enabled = false;
+        self._idle.started = undefined;
+        clearTimeout(self._tmrKeepalive);
+  
+        self._curReq = undefined;
+  
+        if (self._queue.length === 0
+            && self._config.keepalive
+            && self.state === 'authenticated'
+            && !self._idle.enabled) {
+          self._idle.enabled = true;
+          if (m[1] === 'NOOP')
+            self._doKeepaliveTimer();
+          else
+            self._doKeepaliveTimer(true);
+        }
+  
+        self._processQueue();
+      }
+    });
+  
+    this._tmrConn = setTimeout(function() {
+      var err = new Error('Timed out while connecting to server');
+      err.source = 'timeout';
+      self.emit('error', err);
+      socket.destroy();
+    }, config.connTimeout);
+  
+    if (!config.proxy) {
+      socket.connect({
+        port: config.port,
+        host: config.host,
+        localAddress: config.localAddress
+      });
+    }
+};
+
 
 Connection.prototype.serverSupports = function(cap) {
   return (this._caps && this._caps.indexOf(cap) > -1);
